8332168260af828108ea9d293bd3ce9ddccb80db
Semakin Grigory1991 <semakingrigory@yandex.ru>
2023-07-15 13:02:14 +0700

    Initial commit

---
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.netology.nmedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("Hello nMedia","onCreate:")
+    }
+}
